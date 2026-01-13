2169dcd40f4221416adfc0739af011e28b8d3370
Amu-Lee Beukes <amuleebm2024@gmail.com>
2026-01-13 21:00:30 +0200

    Add task feature with menu

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello internship world!")
+tasks = []
+
+def show_menu():
+    print("\n Task Manager")
+    print("1. Add task")
+    print("2. View task")
+    print("3. Exit")
+
+def add_task():
+    task = input("Enter task:")
+    tasks.append(task)
+    print("Task added!")
+
+def main():
+    while True:
+        show_menu()
+        choice = input("choose an option:")
+
+        if choice == "1":
+            add_task()
+        elif choice == "2":
+            print(tasks)
+        elif choice =="3":
+            print("Goodbye !")
+            break
+        else:
+            print("Invalid choice")
+
+        
+main()
+
